27ea29229c4da2c29748253a3de047659d1b647a
Mark Sabotta <mark.sabotta@gmail.com>
2023-10-03 12:55:20 -0600

    adding express centers

---
+++ express-routing/app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+const ExpressError = require('./expressError');
+
+const { convertAndValidateNumsArray, findMode, findMean, findMedian } = require('./helpers');
+
+app.get('/mean', function(req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError('You must pass a list of nums to the query')
+    }
+    let numsString = req.query.nums.split(',');
+    let nums = convertAndValidateNumsArray(numsString);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+
+    let result = {
+        operation: "mean",
+        result: findMean(nums)
+    }
+
+    return res.send(result);
+});
+
+app.get('/median', function(req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError('You must pass a list of nums to the query')
+    }
+
+    let numsString = req.query.nums.split(',');
+    let nums = convertAndValidateNumsArray(numsString);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+
+    let result = {
+        operation: "median",
+        result: findMedian(nums)
+    }
+
+    return res.send(result);
+});
+
+
+app.get('/mode', function(req, res, next) {
+    if (!req.query.nums) {
+        throw new ExpressError('You must pass a list of nums to the query')
+    }
+
+    let numsString = req.query.nums.split(',');
+    let nums = convertAndValidateNumsArray(numsString);
+    if (nums instanceof Error) {
+        throw new ExpressError(nums.message);
+    }
+
+
+    let result = {
+        operation: "mode",
+        result: findMode(nums)
+    }
+
+    return res.send(result);
+});
+
+
+/** error handlers */
+
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found", 404);
+
+    //pass the error to the next function
+    return next(err);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+
+    return res.json({
+        error: err,
+        message: err.message
+    });
+});
+
+app.listen(3000, function() {
+    console.log(`Server starting on port 3000`);
+});
